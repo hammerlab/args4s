fc2ddda822cd31ded1dbe540ca09c0e582d2a58a
Ryan Williams <ryan.blake.williams@gmail.com>
2016-11-13 19:40:15 +0000

    port handlers from magic-rdds, guacamole

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "args4s"
+
+version := "1.0.0"
+
+libraryDependencies ++= Seq(
+  "args4j" % "args4j" % "2.33"
+)
---
+++ src/main/scala/org/hammerlab/args4s/StringOptionHandler.scala
@@ -0,0 +1,21 @@
+package org.hammerlab.args4s
+
+import org.kohsuke.args4j.spi.{OptionHandler, Parameters, Setter}
+import org.kohsuke.args4j.{CmdLineParser, OptionDef, OptionHandlerRegistry}
+
+/**
+ * Args4J option handler that populates an [[Option[String]]].
+ */
+class StringOptionHandler(parser: CmdLineParser, option: OptionDef, setter: Setter[Option[String]])
+  extends OptionHandler[Option[String]](parser, option, setter) {
+  override def getDefaultMetaVariable: String = "path"
+
+  override def parseArguments(params: Parameters): Int = {
+    setter.addValue(Some(params.getParameter(0)))
+    1
+  }
+}
+
+object StringOptionHandler {
+  OptionHandlerRegistry.getRegistry.registerHandler(classOf[Option[String]], classOf[StringOptionHandler])
+}
