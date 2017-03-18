c319f4a31b5415a85015ea6c9f7218b94f8cc7c6
Ryan Williams <ryan.blake.williams@gmail.com>
2017-03-18 17:38:47 +0000

    add usage test

--- src/test/scala/org/hammerlab/args4s/ArgsTest.scala
+++ src/test/scala/org/hammerlab/args4s/ArgsTest.scala
@@ -1,5 +1,7 @@
 package org.hammerlab.args4s
 
+import java.io.ByteArrayOutputStream
+
 import org.apache.hadoop.fs.Path
 import org.hammerlab.test.Suite
 import org.kohsuke.args4j
@@ -10,38 +12,44 @@ import scala.collection.JavaConversions._
 class Args {
   @args4j.Option(
     name = "--str",
-    handler = classOf[StringOptionHandler]
+    handler = classOf[StringOptionHandler],
+    usage = "An optional string"
   )
   var strOpt: Option[String] = None
 
   @args4j.Option(
     name = "--int",
-    handler = classOf[IntOptionHandler]
+    handler = classOf[IntOptionHandler],
+    usage = "An int"
   )
   var intOpt: Option[String] = None
 
   @args4j.Option(
     name = "--path",
-    handler = classOf[PathHandler]
+    handler = classOf[PathHandler],
+    usage = "A path"
   )
   var path: Path = _
 
   @args4j.Option(
     name = "--path-opt",
-    handler = classOf[PathOptionHandler]
+    handler = classOf[PathOptionHandler],
+    usage = "An optional path"
   )
   var pathOpt: Option[Path] = None
 
   @args4j.Option(
     name = "--strings",
-    handler = classOf[StringsOptionHandler]
+    handler = classOf[StringsOptionHandler],
+    usage = "Some strings"
   )
   var strings: Array[String] = Array()
 
   @args4j.Argument(
     metaVar = "<PATHS>",
     multiValued = true,
-    handler = classOf[PathHandler]
+    handler = classOf[PathHandler],
+    usage = "Path arguments"
   )
   var paths: Array[Path] = Array()
 }
@@ -51,6 +59,20 @@ class ArgsTest
   test("parse") {
     val args = new Args
     val parser = new CmdLineParser(args)
+
+    val ss = new ByteArrayOutputStream
+    parser.printUsage(ss)
+    ss.toString("UTF-8") should be(
+      """ <PATHS>            : Path arguments
+        | --int INT          : An int (default: None)
+        | --path PATH        : A path
+        | --path-opt PATH    : An optional path (default: None)
+        | --str VAL          : An optional string (default: None)
+        | --strings STRING[] : Some strings
+        |"""
+      .stripMargin('|')
+    )
+
     parser.parseArgument(
       Seq(
         "--str", "abc",
