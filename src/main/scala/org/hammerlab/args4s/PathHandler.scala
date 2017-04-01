e5636688f43759568c0709d2ccafc322c6cb1c5d
Ryan Williams <ryan.blake.williams@gmail.com>
2017-04-01 23:32:04 +0000

    use Paths

--- src/main/scala/org/hammerlab/args4s/PathHandler.scala
+++ src/main/scala/org/hammerlab/args4s/PathHandler.scala
@@ -1,16 +1,36 @@
 package org.hammerlab.args4s
 
-import org.apache.hadoop.fs.Path
+import java.nio.file.{ Paths, Path ⇒ JPath }
+
+import org.hammerlab.paths.Path
 import org.kohsuke.args4j.spi.Setter
 import org.kohsuke.args4j.{ CmdLineParser, OptionDef }
 
+/**
+ * Since [[Path]] is an [[AnyVal]], options with type [[Path]] end up as type [[java.nio.file.Path]] at run-time, and
+ * should be used with this [[Handler]].
+ */
+class JPathHandler(parser: CmdLineParser,
+                   option: OptionDef,
+                   setter: Setter[JPath])
+  extends Handler[JPath](
+      parser,
+      option,
+      setter,
+      "PATH",
+      Paths.get(_)
+  )
+
+/**
+ * Options that have type [[Array[Path]]] will box [[Path]]s, and so should be fed this [[Handler]].
+ */
 class PathHandler(parser: CmdLineParser,
                   option: OptionDef,
                   setter: Setter[Path])
   extends Handler[Path](
-    parser,
-    option,
-    setter,
-    "PATH",
-    new Path(_)
+        parser,
+        option,
+        setter,
+        "PATH",
+        Path(_)
   )
